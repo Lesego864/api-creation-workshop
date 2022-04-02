8f1ea94af9326f903911ce6e5ee6cb4969c24a37
Lesego864 <tshipulesego@gmail.com>
2022-04-03 01:40:01 +0200

    Added the codes needed for the Missy-Tee api exercise, including installing express, setting the dataset, adding end points, adding routes and filters to the routes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 let seasonFilter = 'All';
 let genderFilter = 'All';
 
+
 const seasonOptions = document.querySelector('.seasons');
 const genderOptions = document.querySelector('.genders');
 const searchResultsElem = document.querySelector('.searchResults');
@@ -10,36 +11,36 @@ const showPriceRangeElem = document.querySelector('.showPriceRange');
 const garmentsTemplateText = document.querySelector('.garmentListTemplate');
 const garmentsTemplate = Handlebars.compile(garmentsTemplateText.innerHTML);
 
-seasonOptions.addEventListener('click', function(evt){
-	seasonFilter = evt.target.value;
-	filterData();
+seasonOptions.addEventListener('click', function(evt) {
+    seasonFilter = evt.target.value;
+    filterData();
 });
 
-genderOptions.addEventListener('click', function(evt){
-	genderFilter = evt.target.value;
-	filterData();
+genderOptions.addEventListener('click', function(evt) {
+    genderFilter = evt.target.value;
+    filterData();
 });
 
 function filterData() {
-	axios
-		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
-		.then(function(result) {
-			searchResultsElem.innerHTML = garmentsTemplate({
-				garments : result.data.garments
-			})
-		});
+    axios
+        .get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
+        .then(function(result) {
+            searchResultsElem.innerHTML = garmentsTemplate({
+                garments: result.data.garments
+            })
+        });
 }
 
-priceRangeElem.addEventListener('change', function(evt){
-	const maxPrice = evt.target.value;
-	showPriceRangeElem.innerHTML = maxPrice;
-	axios
-		.get(`/api/garments/price/${maxPrice}`)
-		.then(function(result) {
-			searchResultsElem.innerHTML = garmentsTemplate({
-				garments : result.data.garments
-			})
-		});
+priceRangeElem.addEventListener('change', function(evt) {
+    const maxPrice = evt.target.value;
+    showPriceRangeElem.innerHTML = maxPrice;
+    axios
+        .get(`/api/garments/price/${maxPrice}`)
+        .then(function(result) {
+            searchResultsElem.innerHTML = garmentsTemplate({
+                garments: result.data.garments
+            })
+        });
 });
 
 filterData();
